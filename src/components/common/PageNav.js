25eeaa5cd78846b5c941e514cb6a5b7fb6747c41
billydean <billy.goehring@gmail.com>
2022-06-27 10:25:44 -0700

    BL-69 added PageNav component styled after ant docs; did initial work on restructuring overall repo organization (mostly empty files/folders)

--- src/components/common/PageNav.js
+++ src/components/common/PageNav.js
@@ -1,5 +1,21 @@
 import { Pagination } from 'antd';
 
-const PageNav = () => <Pagination defaultCurrent={1} total={50} />;
+// dummy for now; variable determined elsewhere:
+// big screen or little screen?
+const bigScreen = true;
+
+const PageNav = () => (
+  <Pagination
+    // mini-fied version of page nav bar
+    size={'small'}
+    // starting displayed page of results
+    defaultCurrent={1}
+    // total TBD by requested data
+    total={50}
+    // # of items per page responsive to device/screen size
+    pageSize={bigScreen ? '40' : '15'}
+    showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
+  />
+);
 
 export default PageNav;
---
+++ src/components/common/TempPageContainer.js
@@ -0,0 +1,1 @@
+// code to be inserted in PageNav's parent, so that screen size can be passed down as a prop (or passed wherever else needed if needed)
