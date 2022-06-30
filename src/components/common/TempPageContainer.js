e3f3f27c433b1514316e64a4f416e4636ca81018
Joshmclemore <josh_mclemore@yahoo.com>
2022-06-30 10:15:14 -0500

    Added state managment and React Hook for the screen size reading feature, needed for pagination. Still need to connect it to Redux

--- src/components/common/TempPageContainer.js
+++ src/components/common/TempPageContainer.js
@@ -1,1 +1,21 @@
 // code to be inserted in PageNav's parent, so that screen size can be passed down as a prop (or passed wherever else needed if needed)
+import { useEffect, useState } from 'react';
+const [bigScreen, setBigScreen] = useState(true); // eslint-disable-line
+
+const windowSize = window.innerWidth;
+
+console.log('window size:', windowSize);
+
+// if windowSize is below 700 then bigScreen is set to false and the "defaultPageSize" changes from 40 to 15
+const ScreenSizeReader = () =>
+  useEffect(() => {
+    if (windowSize < 700) {
+      setBigScreen(false);
+    } else {
+      setBigScreen(true);
+    }
+  }, []);
+
+export default ScreenSizeReader;
+
+// if this isn't read from state, then I don't believe it will affect the render - needs to be plugged into React
