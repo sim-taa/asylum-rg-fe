05cb66e217e303612d04a0e890cdaad05d0f5ca4
XavierTwitty <x.twitty@gmail.com>
2022-07-12 13:59:46 -0700

    renderTable test WIP cleared page

---
+++ src/__tests__/RenderTable.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import testData from '.././data/data';
+import RenderTable from '.././components/pages/Table/RenderTableContainer';
+import { render, cleanup } from '@testing-library/react';
