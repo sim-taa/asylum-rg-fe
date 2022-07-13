5aff5c1163774176506ddb49758d29b69cfd1fad
XavierTwitty <x.twitty@gmail.com>
2022-07-13 08:23:14 -0700

    empty test passing false positive

--- src/__tests__/RenderTable.test.js
+++ src/__tests__/RenderTable.test.js
@@ -2,3 +2,14 @@ import React from 'react';
 import testData from '.././data/data';
 import RenderTable from '.././components/pages/Table/RenderTableContainer';
 import { render, cleanup } from '@testing-library/react';
+import { afterEach, describe } from 'jest-circus';
+
+afterEach(() => {
+  cleanup();
+  jest.restoreAllMocks();
+  jest.clearAllMocks();
+});
+
+describe('<RenderTable/> test suit', () => {
+  test('table is rendered to the screen with data', () => {});
+});
