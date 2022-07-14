6eea49e84b8ceba390607b38eb23f40294efe7a9
unknown <ryancare@protonmail.com>
2022-07-14 19:33:16 -0400

    Evidently, my attempts to add individual files for each push have not been working, so I'm adding them all now.

--- src/components/pages/Table/DateFormatSelect.js
+++ src/components/pages/Table/DateFormatSelect.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { setDateFilterFormat } from '../../../state/actions';
 import 'antd/dist/antd.css';
 import { Select } from 'antd';
 import '../../../styles/FilterMenuStyles/DateFormatSelect.less';
 
 const { Option } = Select;
 
-const handleChange = value => {
-  console.log(`selected ${value}`);
-};
+const mapStateToProps = state => ({
+  isFiscalYear: state.filterReducer.isFiscalYear,
+});
 
-const DateFormatSelect = () => (
-  <Select bordered={false} defaultValue="calendarYear" onChange={handleChange}>
+const DateFormatSelect = ({ isFiscalYear, setDateFilterFormat }) => (
+  <Select
+    bordered={false}
+    value={isFiscalYear ? 'fiscalYear' : 'calendarYear'}
+    onChange={() => setDateFilterFormat(!isFiscalYear)}
+  >
     <Option value="calendarYear">Calendar Year</Option>
     <Option value="fiscalYear">Fiscal Year</Option>
   </Select>
 );
 
-export default DateFormatSelect;
+export default connect(mapStateToProps, { setDateFilterFormat })(
+  DateFormatSelect
+);
