db07f3c353ed602dea20ff7c124ac1a74491496c
EstrellaDionis <EstrellaDionis@gmail.com>
2022-07-15 13:25:26 -0400

    created 3 passing tests in RenderTable.test.js file

--- src/__tests__/RenderTable.test.js
+++ src/__tests__/RenderTable.test.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import data from '.././data/data';
+import { data } from '.././data/data';
 import RenderTable from '.././components/pages/Table/RenderTableContainer';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { describe } from 'jest-circus';
 
-// describe("Render table data to the screen", () => {
-//     test("[1] asylum office is a string", () => {
-//         const input = "AyS";
-//         const expected = "AyS";
-//         const actual = "";
-//         expect.stringContaining(input);
-//     });
-// });
+describe('Render table data to the screen', () => {
+  test('[1] asylum office is a string', () => {
+    const Testdata = data;
+    expect(typeof Testdata.asylumOffice).toBe('string');
+  });
+});
 
 describe('Citizenship data is a string', () => {
   test('[2] Citizenship column is a string', () => {
     const Testdata = data;
-    // const expected = { citizenship: "h" };
-    // const actual = screen.getByText(data.citizenship);
     expect(typeof Testdata.citizenship).toBe('string');
+  });
+});
+
+describe('Race or Ethnicity data is a string', () => {
+  test('[3] Citizenship column is a string', () => {
+    const Testdata = data;
+
+    expect(typeof Testdata.raceOrEthnicity).toBe('string');
   });
 });
