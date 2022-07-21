3a72b260b08e1f7dcccab386fec668d1a0e0149c
EstrellaDionis <EstrellaDionis@gmail.com>
2022-07-21 11:21:38 -0400

    removed testData to data for leaner code, in the process of implementing test for rendering table and data on it

--- src/__tests__/DataShape.test.js
+++ src/__tests__/DataShape.test.js
@@ -1,22 +1,29 @@
 import { data } from '../data/data';
+import { render, screen } from '@testing-library/react';
+import TablePage from '../components/pages/Table';
 
 describe('Asylum Office is a string', () => {
   test('[1] asylumOffice is a string', () => {
-    const Testdata = data;
-    expect(typeof Testdata.asylumOffice).toBe('string');
+    expect(typeof data.asylumOffice).toBe('string');
   });
 });
 
 describe('Citizenship data is a string', () => {
   test('[2] citizenship is a string', () => {
-    const Testdata = data;
-    expect(typeof Testdata.citizenship).toBe('string');
+    expect(typeof data.citizenship).toBe('string');
   });
 });
 
 describe('Race or Ethnicity data is a string', () => {
   test('[3] raceOrEthnicity is a string', () => {
-    const Testdata = data;
-    expect(typeof Testdata.raceOrEthnicity).toBe('string');
+    expect(typeof data.raceOrEthnicity).toBe('string');
+  });
+});
+
+describe('Table renders to the screen with data', () => {
+  test('[4] Table renders to the screen with data', () => {
+    render(<TablePage />);
+    const asylumOfficeString = screen.getByText(/AyS/i);
+    expect(asylumOfficeString).toBeInTheDocument();
   });
 });
