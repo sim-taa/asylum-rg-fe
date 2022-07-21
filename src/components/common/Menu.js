3d69064082e3ea06c2272b4c78b3d55ceff92bc9
billydean <billy.goehring@gmail.com>
2022-07-20 19:44:30 -0700

    BL-469 comment and clean up for hand-off

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,3 +1,8 @@
+// took the time to populate this Menu component from antd
+// because we're already waiting on some merges to go through,
+// I chose not to create yet another 'temporary container' to test/render
+// used codepen instead as an ad-hoc alternative
+
 import { Menu } from 'antd';
 import React from 'react';
 function getItem(label, key, children, type) {
@@ -369,36 +374,6 @@ const items = [
       'init2'
     ),
   ]),
-  //   getItem('Navigation One', 'sub1', <MailOutlined />, [
-  //     getItem(
-  //       'Item 1',
-  //       'g1',
-  //       null,
-  //       [getItem('Option 1', '1'), getItem('Option 2', '2')],
-  //       'group'
-  //     ),
-  //     getItem(
-  //       'Item 2',
-  //       'g2',
-  //       null,
-  //       [getItem('Option 3', '3'), getItem('Option 4', '4')],
-  //       'group'
-  //     ),
-  //   ]),
-  //   getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-  //     getItem('Option 5', '5'),
-  //     getItem('Option 6', '6'),
-  //     getItem('Submenu', 'sub3', null, [
-  //       getItem('Option 7', '7'),
-  //       getItem('Option 8', '8'),
-  //     ]),
-  //   ]),
-  //   getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-  //     getItem('Option 9', '9'),
-  //     getItem('Option 10', '10'),
-  //     getItem('Option 11', '11'),
-  //     getItem('Option 12', '12'),
-  //   ]),
 ];
 const App = () => {
   const onClick = e => {
