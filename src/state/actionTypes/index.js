fa3f21bffc43574189807e5840f2adbbe8682d1e
unknown <ryancare@protonmail.com>
2022-07-23 14:18:52 -0400

    BL-72 Updated actionTypes to match BL-635 without removing soon-to-be-deprecated types

--- src/state/actionTypes/index.js
+++ src/state/actionTypes/index.js
@@ -1,8 +1,18 @@
 export const GET_DATA = 'GET_DATA';
-export const FILTER_SEARCH = 'FILTER_SEARCH';
 export const RESET_DATA = 'RESET_DATA';
+
+//These are for filtering/searching data already retrieved from the API
 export const SHOW_ADVANCED_SEARCH = 'SHOW_ADVANCED_SEARCH';
 export const ADVANCED_SEARCH = 'ADVANCED_SEARCH';
+export const FILTER_SEARCH = 'FILTER_SEARCH';
+export const RESET_CASE_DATA = 'RESET_DATA';
+//Actions to choose filters and fetch API data
+export const GET_FILTERED_DATA = 'GET_FILTERED_DATA';
+export const GET_DEFAULT_COMPARISONS = 'GET_DEFAULT_COMPARISONS';
 export const SET_DATE_FILTER_FORMAT = 'SET_DATE_FILTER_FORMAT';
 export const SET_ASYLUM_OFFICE_FILTER = 'SET_ASYLUM_OFFICE_FILTER';
 export const SET_CONTINENT_FILTER = 'SET_CONTINENT_FILTER';
+export const SET_GEOPOLITICAL_FILTER = 'SET_GEOPOLITICAL_FILTER';
+//This is for use until the proper API is up and running, though it may
+//Prove valuable for testing purposes even after
+export const GET_MOCK_FILTERED_DATA = 'GET_MOCK_FILTERED_DATA';
