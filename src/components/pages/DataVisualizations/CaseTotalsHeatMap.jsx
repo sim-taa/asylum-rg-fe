776b2bc6c93bb97e928a40e4d1069a9b4ed84633
unknown <ryancare@protonmail.com>
2022-07-24 14:03:45 -0400

    BL-TBD Created potential data object schema to be received from the API and added random data generation to test it out. Added presenting data by year or by totals. Split data presentation into outcomes to enable multiple heat map formats from a single API data response.

--- src/components/pages/DataVisualizations/CaseTotalsHeatMap.jsx
+++ src/components/pages/DataVisualizations/CaseTotalsHeatMap.jsx
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Plot from 'react-plotly.js';
 import { regions } from '../../../data/filterConstants';
 
+//Auto-generate a data-values object to simulate what the API could return
+const apiData = regions.map(region => {
+  return {
+    location: region.territory,
+    caseTotals: [
+      {
+        year: 2018,
+        grant: Math.floor(Math.random() * 100),
+        deny: Math.floor(Math.random() * 100),
+        dismiss: Math.floor(Math.random() * 100),
+      },
+      {
+        year: 2019,
+        grant: Math.floor(Math.random() * 100),
+        deny: Math.floor(Math.random() * 100),
+        dismiss: Math.floor(Math.random() * 100),
+      },
+      {
+        year: 2020,
+        grant: Math.floor(Math.random() * 100),
+        deny: Math.floor(Math.random() * 100),
+        dismiss: Math.floor(Math.random() * 100),
+      },
+      {
+        year: 2021,
+        grant: Math.floor(Math.random() * 100),
+        deny: Math.floor(Math.random() * 100),
+        dismiss: Math.floor(Math.random() * 100),
+      },
+    ],
+  };
+});
+
 const CaseTotalsHeatMap = props => {
+  const [yearSelection, setYearSelection] = useState(2018);
+
   return (
     <Plot
       data={[
         {
           type: 'choropleth',
           locationmode: 'country names',
-          locations: regions.map(region => region.territory),
-          z: regions.map(region => Math.floor(Math.random() * 200)),
-          text: regions.map(region => region.territory),
+          locations: apiData.map(region => region.location),
+          z: apiData.map(region =>
+            yearSelection === 'Total'
+              ? region.caseTotals.reduce(
+                  (current, next) =>
+                    current + next.grant + next.deny + next.dismiss,
+                  0
+                )
+              : region.caseTotals
+                  .filter(dataYear => yearSelection === dataYear.year)
+                  .reduce(
+                    (current, next) =>
+                      current + next.grant + next.deny + next.dismiss,
+                    0
+                  )
+          ),
+          text: apiData.map(region => region.location),
           autocolorscale: true,
         },
       ]}
