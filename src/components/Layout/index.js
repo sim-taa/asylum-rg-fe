7644c2faa8494c2e602d07bed00bd57f94e73511
ricardo <ricky032929@gmail.com>
2022-07-26 11:43:58 -0400

    added layoutStyles folder

---
+++ src/components/Layout/index.js
@@ -0,0 +1,3 @@
+import NavBar from './NavBar';
+import { FooterContent } from './Footer';
+export { NavBar, FooterContent };
---
+++ src/components/pages/AdvancedApiFilter/ContinentSelect.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { setContinentFilter } from '../../../state/actionCreators';
+import 'antd/dist/antd.css';
+import { Select } from 'antd';
+import { continentEnum } from '../../../data/filterConstants';
+const { Option } = Select;
+
+const mapStateToProps = state => ({
+  continents: state.fetchFilterReducer.continents,
+});
+
+const continentOptions = Object.values(continentEnum).map(continent => (
+  <Option key={continent}>{continent}</Option>
+));
+
+const ContinentSelect = ({ continents, setContinentFilter }) => {
+  const dispatchChange = continentArray => {
+    setContinentFilter(
+      typeof continentArray === 'string' ? [continentArray] : continentArray
+    );
+  };
+  return (
+    <Select
+      mode="multiple"
+      allowClear
+      style={{
+        textAlign: 'center',
+        width: '15em',
+      }}
+      placeholder="Filter By Continent"
+      defaultValue={[]}
+      onChange={dispatchChange}
+    >
+      {continentOptions}
+    </Select>
+  );
+};
+
+export default connect(mapStateToProps, { setContinentFilter })(
+  ContinentSelect
+);
