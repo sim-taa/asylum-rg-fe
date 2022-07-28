52a0b1072e2f5249ad6457f03243564b5ba95712
unknown <ryancare@protonmail.com>
2022-07-28 17:59:37 -0400

    BL-TBD Added vizReducer to the combinereducers function

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import dataReducer from './dataReducer';
 import fetchFilterReducer from './fetchFilterReducer';
+import vizReducer from './vizReducer';
 
 export default combineReducers({
   dataReducer,
   fetchFilterReducer,
+  vizReducer,
 });
