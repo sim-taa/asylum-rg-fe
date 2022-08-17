91b2f5b78140abf95ca831291b071df6149b9e80
Mack Gallagher <wingsonthebus@gmail.com>
2022-08-17 00:25:42 -0500

    initial replacement of files in DataVisualizations

--- src/components/pages/DataVisualizations/HeatMapContainer.jsx
+++ src/components/pages/DataVisualizations/HeatMapContainer.jsx
@@ -1,35 +1,10 @@
 import React from 'react';
-import WorldHeatMap from './HeatMaps/WorldHeatMap';
-import { connect } from 'react-redux';
-import { setHeatMapYear } from '../../../state/actionCreators';
+import CitizenshipMapAll from './HeatMaps/CitizenshipMapAll';
 import 'antd/dist/antd.css';
 import { Select } from 'antd';
 const { Option } = Select;
 
-const mapStateToProps = state => ({
-  heatMapYear: state.vizReducer.heatMapYear,
-  heatMapData: state.vizReducer.heatMapData,
-});
-
-const HeatMapContainer = ({ heatMapYear, heatMapData, setHeatMapYear }) => {
-  //These reducer functions are passed to the component and used to process
-  //incoming API data into case data arrays. Defining these functions outside
-  //of the individual heatmap components allows for reusability
-  const reduceTotals = region =>
-    region.reduce(
-      (current, next) => current + next.grant + next.deny + next.dismiss,
-      0
-    );
-  const reduceGrantRate = region => {
-    let grant = 0,
-      deny = 0;
-    region.forEach(year => {
-      grant += year.grant;
-      deny += year.deny;
-    });
-    return Math.floor((100 * grant) / (grant + deny));
-  };
-
+function HeatMapContainer() {
   return (
     <div
       style={{
@@ -48,11 +23,10 @@ const HeatMapContainer = ({ heatMapYear, heatMapData, setHeatMapYear }) => {
           }}
           placeholder="Filter By Office Region"
           defaultValue={'all'}
-          onChange={value => setHeatMapYear(value)}
         >
           {/* Ultimately, as the data shape is better known, these should be dynamically created */}
-          <Option value={'all'}>Display Year: All</Option>
-          <Option value={2021}>Display Year: 2021</Option>
+          <Option value={'all'}>Data for Office: All</Option>
+          <Option value={2021}>Data for Office: </Option>
           <Option value={2020}>Display Year: 2020</Option>
           <Option value={2019}>Display Year: 2019</Option>
           <Option value={2018}>Display Year: 2018</Option>
@@ -66,29 +40,10 @@ const HeatMapContainer = ({ heatMapYear, heatMapData, setHeatMapYear }) => {
           backgroundColor: '#f7e4ca',
         }}
       >
-        <WorldHeatMap
-          yearSelection={heatMapYear}
-          reducerForm={reduceTotals}
-          apiData={heatMapData}
-          title={
-            heatMapYear === 'all'
-              ? 'Total Asylum Petitions 2018-2021'
-              : `Asylum Petitions In ${heatMapYear}`
-          }
-        />
-        <WorldHeatMap
-          yearSelection={heatMapYear}
-          reducerForm={reduceGrantRate}
-          apiData={heatMapData}
-          title={
-            heatMapYear === 'all'
-              ? 'Total Asylum Grant Rate % 2018-2021'
-              : `Asylum Grant Rate % For ${heatMapYear}`
-          }
-        />
+        <CitizenshipMapAll />
       </div>
     </div>
   );
-};
+}
 
-export default connect(mapStateToProps, { setHeatMapYear })(HeatMapContainer);
+export default HeatMapContainer;
---
+++ src/components/pages/DataVisualizations/HeatMaps/CitizenshipMapAll.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+function CitizenshipMapAll() {
+  return (
+    <div className="citizenship-map-all-container">
+      {'CITIZENSHIP MAP WILL GO HERE'}
+    </div>
+  );
+}
+
+export default CitizenshipMapAll;
